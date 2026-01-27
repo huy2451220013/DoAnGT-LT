aef6a4232685f44f25fb14bb776893429b279f1b
Tran Quoc Huy <huy_2451220013@dau.edu.vn>
2026-01-27 16:32:38 +0700

    bai12

---
+++ bai12.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+#include <functional> 
+
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        std::function<void(int, int)> quickSort = [&](int left, int right) {
+            if (left >= right) {
+                return;
+            }
+            int pivotIndex = left + (right - left) / 2; 
+            int pivotValue = nums[pivotIndex];
+            int i = left - 1;
+            int j = right + 1;
+            while (i < j) {
+                do {
+                    i++;
+                } while (nums[i] < pivotValue);
+                do {
+                    j--;
+                } while (nums[j] > pivotValue);
+                if (i < j) {
+                    std::swap(nums[i], nums[j]);
+                }
+            }
+            quickSort(left, j); 
+            quickSort(j + 1, right); 
+        };
+        quickSort(0, nums.size() - 1);
+        return nums;
+    }
+};
