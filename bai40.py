1e5ed70e7438f2195a33124b02159f2fa35076dd
Tran Quoc Huy <huy_2451220013@dau.edu.vn>
2026-02-02 23:52:53 +0700

    bai40

---
+++ bai40.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def sortPeople(self, names, heights):
+        for _ in range(len(names)-1):
+            for i in range(len(names)-1):
+                if heights[i] < heights[i+1]:
+                    names[i],names[i+1] = names[i+1],names[i]
+                    heights[i],heights[i+1] = heights[i+1],heights[i]
+
+        return names
+        
